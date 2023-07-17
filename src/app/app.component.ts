2b1f6d39f1f7a8e99c5b4c05bc7ad8b31e724ead
davilnv <davilima871@gmail.com>
2023-07-17 07:39:36 -0300

    Study of interpolation, event binding and reference variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Projeto Teste';
+  nome = '';
+
+  alterar(nome: any) {
+    this.nome = nome.target.value;
+  }
+
+  adicionar(nome: string) {
+    this.nome = nome;
+  }
+
 }
